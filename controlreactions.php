93c194ad9e37a2a24cdbc0b753fd45e929a484bb
Ronnald R Machado <rrmachado@id.uff.br>
2018-03-27 11:02:29 -0300

    One vote only - finished

--- controlreactions.php
+++ controlreactions.php
@@ -15,28 +15,27 @@ $record->user = $user;
 
 $vote = $ireactions->has_vote($post,$reaction,$user);
 $reactionid = 0;
+$past_vote = null;
 if($ireactions->has_any_vote($post,$user)){
 	$reactionid = $ireactions->get_last_vote($post, $user);
+        
 }
 
 if ($ireactions->has_any_vote($post,$user)) {
-		$reaction_sql = $DB->get_record($table, array('post'=>$post,'user'=>$user),$fields='reaction',$strictness=IGNORE_MULTIPLE);
+	$reaction_sql = $DB->get_record($table, array('post'=>$post,'user'=>$user),$fields='reaction',$strictness=IGNORE_MULTIPLE);
 
-		$where = array('post' => $post,'reaction'=> $reaction_sql->reaction,'user'=>$user );
 
-        $DB->delete_records($table, $where);
-
-        $DB->insert_record($table, $record, false, false);
+        $ireactions->delete_vote($post,$reaction_sql->reaction,$user);
+        if ($reaction_sql->reaction!=$reaction) {
+                $ireactions->add_vote($record);
+        }
+        
 
 }else{
-        $record = new stdClass();
-        $record->post = $post;
-        $record->reaction = $reaction;
-        $record->user = $user;
-        $DB->insert_record($table, $record, false, false);
+        $ireactions->add_vote($record);
 }
-
-$response = array('votes' => $ireactions->get_votes($post,$reaction),'had_vote'=>$vote,'last_vote'=>$reactionid);
+$past_vote = $ireactions->get_votes($post, $reactionid);
+$response = array('votes' => $ireactions->get_votes($post,$reaction),'had_vote'=>$vote,'last_vote'=>$reactionid,'past_vote'=>$past_vote);
 echo json_encode($response);
 
 
--- reactions.js
+++ reactions.js
@@ -1,26 +1,27 @@
-function vote(foreact,user,post,reaction,votes,hasvote){
-    cname =String(foreact)+String(post)+String(reaction);
-    last_vote = String(foreact)+String(post);
+  function vote(foreact,user,post,reaction,votes,hasvote){
+      cname =String(foreact)+String(post)+String(reaction);
+      last_vote = String(foreact)+String(post);
 
-    parentclass = document.getElementById(cname).parentNode.className;
-    xmlhttp=new XMLHttpRequest();
-    
-        
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState==4 && this.status==200) {
-	       answer=JSON.parse(this.response);
+      parentclass = document.getElementById(cname).parentNode.className;
+      xmlhttp=new XMLHttpRequest();
+      
+          
+      xmlhttp.onreadystatechange = function() {
+          if (this.readyState==4 && this.status==200) {
+  	       answer=JSON.parse(this.response);
 
-           document.getElementById(cname).firstChild.data=' ('+answer.votes+')';
-           
-           if (answer.had_vote) {
-            document.getElementById('btn'+cname).className ='btn btn-default btn-sm';
-           }else{
-            
-            document.getElementById('btn'+last_vote+answer.last_vote).className ='btn btn-default btn-sm';
-            document.getElementById('btn'+cname).className ='btn btn-primary btn-sm';
-           }
-	}
-}; 
-        xmlhttp.open("GET","controlreactions.php?post="+post+"&reaction="+reaction+"&user="+user+"&pc="+parentclass,true);
-        xmlhttp.send();
-}
+             document.getElementById(cname).firstChild.data=' ('+answer.votes+')';
+             document.getElementById('btn'+cname).className ='btn btn-primary btn-sm';
+             if (answer.had_vote) {
+              document.getElementById(last_vote+answer.last_vote).firstChild.data = '('+answer.past_vote+')';
+              document.getElementById('btn'+last_vote+answer.last_vote).className ='btn btn-default btn-sm';
+              document.getElementById('btn'+cname).className ='btn btn-default btn-sm';
+             }else{
+              document.getElementById(last_vote+answer.last_vote).firstChild.data = '('+answer.past_vote+')';
+              document.getElementById('btn'+last_vote+answer.last_vote).className ='btn btn-default btn-sm';
+             }
+  	}
+  }; 
+          xmlhttp.open("GET","controlreactions.php?post="+post+"&reaction="+reaction+"&user="+user+"&pc="+parentclass,true);
+          xmlhttp.send();
+  }

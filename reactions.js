3238769dc4e5df0ad5f23c6c3105cbeaf507b224
Ronnald R Machado <rrmachado@id.uff.br>
2018-04-04 11:32:00 -0300

    Add new stack and fix visual issue onclick function

--- reactions.js
+++ reactions.js
@@ -13,11 +13,11 @@
              document.getElementById(cname).firstChild.data=' ('+answer.votes+')';
              document.getElementById('btn'+cname).className ='btn btn-primary btn-sm';
              if (answer.had_vote) {
-              document.getElementById(last_vote+answer.last_vote).firstChild.data = '('+answer.past_vote+')';
+              document.getElementById(last_vote+answer.last_vote).firstChild.data = ' ('+answer.past_vote+')';
               document.getElementById('btn'+last_vote+answer.last_vote).className ='btn btn-default btn-sm';
               document.getElementById('btn'+cname).className ='btn btn-default btn-sm';
              }else{
-              document.getElementById(last_vote+answer.last_vote).firstChild.data = '('+answer.past_vote+')';
+              document.getElementById(last_vote+answer.last_vote).firstChild.data = ' ('+answer.past_vote+')';
               document.getElementById('btn'+last_vote+answer.last_vote).className ='btn btn-default btn-sm';
              }
   	}
